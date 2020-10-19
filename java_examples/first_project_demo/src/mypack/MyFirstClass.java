1d2d70533d03badf89e0a3b79e1e2b96c303c7a7
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-10-19 22:13:00 +0530

    notes and first project

---
+++ java_examples/first_project_demo/src/mypack/MyFirstClass.java
@@ -0,0 +1,17 @@
+package mypack;
+
+public class MyFirstClass {
+
+//	public static void main(String[] args) {
+//		
+//		System.out.print("Hello World.....");
+//		System.out.println(" Hello Again");
+//		System.out.println("Hello Once Again");
+//
+//	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hey");
+	}
+
+}
---
+++ java_examples/test_cmd/CustomHello.java
@@ -0,0 +1,9 @@
+package p1;
+public class CustomHello{
+
+public static void main(String a[]){
+System.out.println("Hello from cmd Line");
+
+}
+
+}
